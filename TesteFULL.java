b42716e62147dc4968125adab2a699fa70cdea5a
Rafaelzzz <azeraf@gmail.com>
2015-09-10 22:54:28 -0300

    colocando o menu na frente

--- TesteFULL.java
+++ TesteFULL.java
@@ -24,19 +24,7 @@ public class TesteFULL {
 		Cliente c,k;
 		
 		do{
-		System.out.println("Insira o cpf:");
-        cpf = sc.nextLine();
-		cPf = new Cpf(cpf);    
-        System.out.println("Insira o nome:");
-        nome = sc.nextLine();
-        System.out.println("Insira a renda:");
-        renda = sc.nextFloat();
-        System.out.println("Insira a idade:");
-        idade = sc.nextInt();
-        System.out.println("Insira o sexo(0 = Masculino e 1 = Feminino): ");
-        sexo = sc.nextInt();
-        c = new Cliente(cPf,nome,renda,idade,sexo);
-		
+
 		System.out.println("Digite:\n"
 				+ "1 para incluir\n"
 				+ "2 para excluir\n"
@@ -46,27 +34,78 @@ public class TesteFULL {
 		x = sc.nextInt();
 		sc.nextLine();
 		switch(x){
-		case 1:{ r.incluir(c);
+		case 1:{
+			System.out.println("Insira o cpf:");
+			cpf = sc.nextLine();
+			cPf = new Cpf(cpf);
+			System.out.println("Insira o nome:");
+			nome = sc.nextLine();
+			System.out.println("Insira a renda:");
+			renda = sc.nextFloat();
+			System.out.println("Insira a idade:");
+			idade = sc.nextInt();
+			System.out.println("Insira o sexo(0 = Masculino e 1 = Feminino): ");
+			sexo = sc.nextInt();
+			c = new Cliente(cPf,nome,renda,idade,sexo);
+			r.incluir(c);
 			     break;
 			     }
-		case 2:{ r.excluir(c);
+		case 2:{
+            System.out.println("Insira o cpf:");
+            cpf = sc.nextLine();
+            cPf = new Cpf(cpf);
+            System.out.println("Insira o nome:");
+            nome = sc.nextLine();
+            System.out.println("Insira a renda:");
+            renda = sc.nextFloat();
+            System.out.println("Insira a idade:");
+            idade = sc.nextInt();
+            System.out.println("Insira o sexo(0 = Masculino e 1 = Feminino): ");
+            sexo = sc.nextInt();
+            c = new Cliente(cPf,nome,renda,idade,sexo);
+            r.excluir(c);
 	     		 break;
 	     	   	 }
-		case 3:{ System.out.println(r.buscar(c).toString());
+		case 3:{
+            System.out.println("Insira o cpf:");
+            cpf = sc.nextLine();
+            cPf = new Cpf(cpf);
+            System.out.println("Insira o nome:");
+            nome = sc.nextLine();
+            System.out.println("Insira a renda:");
+            renda = sc.nextFloat();
+            System.out.println("Insira a idade:");
+            idade = sc.nextInt();
+            System.out.println("Insira o sexo(0 = Masculino e 1 = Feminino): ");
+            sexo = sc.nextInt();
+            c = new Cliente(cPf,nome,renda,idade,sexo);
+            System.out.println(r.buscar(c).toString());
 			 	 break;
 			     }
-		case 4:{ System.out.println("Insira o novo cpf:");
-				 novoCpf = sc.nextLine();
-				 novoCPf = new Cpf(novoCpf);
-				 System.out.println("Insira o novo nome:");
-				 novoNome = sc.nextLine();
-				 System.out.println("Insira a nova renda:");
-			     novaRenda = sc.nextFloat();
-			     System.out.println("Insira a nova idade:");
-			     novaIdade = sc.nextInt();
-			     System.out.println("Insira o novo sexo(0 = Masculino e 1 = Feminino: ");
-			     novoSexo = sc.nextInt();
-				 k = new Cliente(novoCPf,novoNome,novaRenda,novaIdade,novoSexo);
+		case 4:{ System.out.println("Insira o cpf:");
+            cpf = sc.nextLine();
+            cPf = new Cpf(cpf);
+            System.out.println("Insira o nome:");
+            nome = sc.nextLine();
+            System.out.println("Insira a renda:");
+            renda = sc.nextFloat();
+            System.out.println("Insira a idade:");
+            idade = sc.nextInt();
+            System.out.println("Insira o sexo(0 = Masculino e 1 = Feminino): ");
+            sexo = sc.nextInt();
+            c = new Cliente(cPf,nome,renda,idade,sexo);
+             System.out.println("Insira o novo cpf:");
+			 novoCpf = sc.nextLine();
+			 novoCPf = new Cpf(novoCpf);
+			 System.out.println("Insira o novo nome:");
+			 novoNome = sc.nextLine();
+			 System.out.println("Insira a nova renda:");
+			 novaRenda = sc.nextFloat();
+			 System.out.println("Insira a nova idade:");
+			 novaIdade = sc.nextInt();
+			 System.out.println("Insira o novo sexo(0 = Masculino e 1 = Feminino: ");
+			 novoSexo = sc.nextInt();
+			 k = new Cliente(novoCPf,novoNome,novaRenda,novaIdade,novoSexo);
 				 r.alterar(c, k);
 				 break;
 			    }
